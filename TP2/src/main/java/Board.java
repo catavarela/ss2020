550217596d2f407986b2af46e611c405efa3eea1
lulitay <ltay@itba.edu.ar>
2020-08-26 01:14:01 -0300

    Added primitive 2D board constructor

--- TP2/src/main/java/Board.java
+++ TP2/src/main/java/Board.java
@@ -1,31 +1,43 @@
-public class Board {
-    Boolean current_board[][] = null;
-    Boolean last_board[][] = null;
-    int size;
+import java.util.Random;
 
-    //TODO: constructor
-    public Board(int size, int percentage) {
+public class Board {
+    private int[][] current_board;
+    private int size;
+
+    //TODO: reducir el espacio donde inician las celdas vivas
+    //TODO: definir el porcentaje de celdas vivas
+    Board(int size, int percentage) {
         this.size = size;
+        current_board = new int[size][size];
+
+        Random rand = new Random();
+
+        for(int row = 0; row < size; row++) {
+            for (int column = 0; column < size; column++) {
+                current_board[row][column] = rand.nextInt(2);
+            }
+        }
     }
 
     public void update() {
         int neighbours;
+        int[][] next_board = new int[size][size];
 
         for(int row = 0; row < size; row++) {
             for(int column = 0; column < size; column++) {
                 neighbours = countNeighbours(row,column);
 
-                if(last_board[row][column] && !(neighbours == 2 || neighbours == 3)) {
-                    current_board[row][column] = false;
-                } else if(!last_board[row][column] && neighbours == 3) {
-                    current_board[row][column] = true;
+                if(current_board[row][column] == 1 && !(neighbours == 2 || neighbours == 3)) {
+                    next_board[row][column] = 0;
+                } else if(current_board[row][column] == 0 && neighbours == 3) {
+                    next_board[row][column] = 1;
                 } else {
-                    current_board[row][column] = last_board[row][column];
+                    next_board[row][column] = current_board[row][column];
                 }
             }
         }
 
-        last_board = current_board;
+        current_board = next_board;
     }
 
     private int countNeighbours(int row, int column) {
@@ -35,17 +47,23 @@ public class Board {
         left = column == 0 ? size - 1 : column - 1;
         right =  column == size - 1 ? 0 : column + 1;
 
-        int count = 0;
-        if(last_board[up][left]) count++;
-        if(last_board[up][column]) count++;
-        if(last_board[up][right]) count++;
-        if(last_board[row][left]) count++;
-        if(last_board[row][right]) count++;
-        if(last_board[down][left]) count++;
-        if(last_board[down][column]) count++;
-        if(last_board[down][right]) count++;
+        return current_board[up][left] +
+               current_board[up][column] +
+               current_board[up][right] +
+               current_board[row][left] +
+               current_board[row][right] +
+               current_board[down][left] +
+               current_board[down][column] +
+               current_board[down][right];
+    }
 
-        return count;
+    public void printBoard() {
+        for(int row = 0; row < size; row++) {
+            for (int column = 0; column < size; column++) {
+                System.out.print(current_board[row][column]);
+            }
+            System.out.print('\n');
+        }
     }
 
 
--- TP2/src/main/java/Main.java
+++ TP2/src/main/java/Main.java
@@ -1,6 +1,13 @@
 public class Main {
     public static void main(String[] args) {
         //TODO: recibir por input el porcentaje de celdas vivas y el tamaño
+
+        Board board = new Board(5,1);
+        board.printBoard();
+        System.out.println("=========================");
+        board.update();
+        board.printBoard();
+        return;
     }
 
 }
