ce1d44461a98d4a985c4f7dfc627d6fc0d9dc782
Catalina Varela Ballesteros <cavarela@itba.edu.ar>
2020-08-29 21:31:26 -0300

    main modificado para que acepte multiples corridas y cree outputs de dichas corridas en csv

--- TP2/src/main/java/Main.java
+++ TP2/src/main/java/Main.java
@@ -7,7 +7,7 @@ import static java.lang.System.exit;
 
 public class Main {
     public static void main(String[] args) {
-        if(args.length < 5) {
+        if(args.length < 6) {
             if(args[0] == null)
                 System.out.println("Falta especificar el tamaño del tablero");
 
@@ -23,6 +23,9 @@ public class Main {
             if(args[4] == null)
                 System.out.println("Falta especificar el número de regla a utilizar");
 
+            if(args[5] == null)
+                System.out.println("Falta especificar la cantidad de corridas");
+
             exit(1);
         }
 
@@ -31,23 +34,49 @@ public class Main {
         int iterations = Integer.parseInt(args[2]);
         boolean three_dimensional = Boolean.parseBoolean(args[3]);
         int rule = Integer.parseInt(args[4]);
-        Board board = new Board(size, percentage, three_dimensional, rule);
-        List<String> output = board.printBoard();
+        int corridas = Integer.parseInt(args[5]);
+
+        int aux_corridas = corridas;
         List<String> aliveCells = new ArrayList<String>();
-        aliveCells.add(String.valueOf(board.getAliveCells()));
         List<String> furthestCells = new ArrayList<String>();
-        furthestCells.add(String.valueOf(board.getFurthestCell()));
+        List<String> output = null;
+
+        while (corridas-- > 0)
+            if(corridas == 0)
+                output = correr(true,aux_corridas-corridas,iterations,size, percentage, three_dimensional, rule, aliveCells, furthestCells);
+            else
+                output = correr(false,aux_corridas-corridas,iterations,size, percentage, three_dimensional, rule, aliveCells, furthestCells);
+
+        //por ahora el output es de la ultima corrida
+        writeFile(output, "output.xyz");
+
+        writeFileCSV(aliveCells, "aliveCells.csv", "Corrida,Regla,Porcentaje,Iteracion,Vivas");
+        writeFileCSV(furthestCells, "furthestCells.csv", "Corrida,Regla,Porcentaje,Iteracion,Lejania");
+    }
+
+    public static List<String> correr(boolean ultima_corrida, int corrida, int iterations, int size, double percentage, boolean three_dimensional, int rule, List<String> aliveCells, List<String> furthestCells){
+        Board board = new Board(size, percentage, three_dimensional, rule);
+        List<String> output = null;
+
+        if(ultima_corrida)
+            output = board.printBoard();
+
+        aliveCells.add(String.valueOf(corrida) + "," + rule + "," + percentage + "," + "0" + "," + String.valueOf(board.getAliveCells()));
+        furthestCells.add(String.valueOf(corrida) + "," + rule + "," + percentage + "," + "0" + String.valueOf(board.getFurthestCell()));
+
+        int aux_iterations = iterations;
 
         while(iterations-- > 0) {
             board.update();
-            output.addAll(board.printBoard());
-            aliveCells.add(String.valueOf(board.getAliveCells()));
-            furthestCells.add(String.valueOf(board.getFurthestCell()));
+
+            if(ultima_corrida)
+                output.addAll(board.printBoard());
+
+            aliveCells.add(String.valueOf(corrida) + "," + rule + "," + percentage + "," + String.valueOf(aux_iterations-iterations) + "," + String.valueOf(board.getAliveCells()));
+            furthestCells.add(String.valueOf(corrida) + "," + rule + "," + percentage + "," + String.valueOf(aux_iterations-iterations) + "," + String.valueOf(board.getFurthestCell()));
         }
 
-        writeFile(output, "output.xyz");
-        writeFile(aliveCells, "aliveCells.txt");
-        writeFile(furthestCells, "furthestCells.txt");
+        return output;
     }
 
     public static void writeFile(List<String> output, String fileName){
@@ -55,6 +84,25 @@ public class Main {
 
         try {
             FileWriter writer = new FileWriter(fileName);
+
+            while(it.hasNext())
+                writer.write(it.next() + '\n');
+
+            writer.close();
+
+        } catch (IOException e) {
+            System.out.println("Ocurrio un error al querer crear/escribir el archivo" + fileName + '.');
+            e.printStackTrace();
+        }
+    }
+
+    public static void writeFileCSV(List<String> output, String fileName, String firstRow){
+        Iterator<String> it = output.iterator();
+
+        try {
+            FileWriter writer = new FileWriter(fileName);
+
+            writer.write(firstRow + '\n');
 
             while(it.hasNext())
                 writer.write(it.next() + '\n');
