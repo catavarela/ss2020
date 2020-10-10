6b7c6058e07ef8245f7ec0f7df6c4222d8082d96
catavarela <cavarela@itba.edu.ar>
2020-10-10 00:29:04 -0300

    Revert "added min distance, and print to term"
    This reverts commit 5db13be8d79c64b4bf1f31f921af6f3ed28b5877.

--- TP4/src/main/java/Constants.java
+++ TP4/src/main/java/Constants.java
@@ -8,7 +8,7 @@ public class Constants {
     public static double earth_mass = 5.97219 * Math.pow(10,24);
     public static double x0_earth = 1.493188929636662 * Math.pow(10,11);
     public static double y0_earth = 1.318936357931255* Math.pow(10,10);
-    public static double vx0_earth = -3.113279917782445 * Math.pow(10,3);
+    public static double vx0_earth = -3.113279917782445 * Math.pow(10,0);
     public static double vy0_earth = 2.955205189256462* Math.pow(10,4);
     public static double mars_mass = 6.4171 * Math.pow(10,23);
     public static double x0_mars = 2.059448551842169* Math.pow(10,11);
